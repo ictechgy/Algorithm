0cabf182f0de7ea5aa3c005d236c12f437220c46
ictechgy <ictechgy@gmail.com>
2020-06-10 17:23:57 +0900

    adding..

--- boj/1065.c
+++ boj/1065.c
@@ -1,21 +1,24 @@
-//한 수 문제
 #include <stdio.h>
 
 int main(void){
-    int num;
+    int i, num, tmp, count = 0, gap, flag; //count - 한수의 개수를 체크, flag - 한수 여부
     scanf("%d", &num);
 
-    int count = 0;      // 한 수의 개수를 체크
-
-    for(int i = 1; i <= num; num++){
-        int oldGap, newGap;
-        while(num){     //num을 234로 가정
-            newGap = num % 10 - num/10%10;
-            num /= 10;
+    for(i = 1; i <= num; i++){
+        tmp = i;
+        flag = 1;   //일단 한수라고 가정
+        gap = tmp % 10 - ((tmp/10)%10); //1의 자리와 10의 자리 차이 계산
+        tmp /= 10;      //한번 계산은 했으므로 10으로 나눔  
+        while(tmp/10 != 0){    //모든 수에 대한 차이 계산 시 종료(한자리 숫자만 남게 되는 경우 더이상 계산할 필요가 없음)
+            if(gap != tmp % 10 - ((tmp/10)%10)){
+                flag = 0;
+                break;
+            }
+            tmp /= 10;
         }
-        if (num % 10 == num /10 % 10 == num / 100 % 10 == num / 1000 % 10)
+        if(flag) count++;
     }
-
+    printf("%d\n", count);
 
     return 0;
 }
