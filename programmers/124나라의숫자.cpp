845f1acde5a2d418e1e22e93e9e44769ab8566ac
ictechgy <ictechgy@gmail.com>
2020-07-02 20:57:56 +0900

    solved but timed out when efficiency tests

--- programmers/124나라의숫자.cpp
+++ programmers/124나라의숫자.cpp
@@ -3,19 +3,22 @@
 
 using namespace std;
 
-string proc_carry(string s, int p){    //carry processing
-    //p인덱스에서 넘어온 캐리를 처리하여 s를 변경시킨다.
+void proc_carry(string& s, int p){    //processe carry, s - 숫자 원문, p - carry가 발생한 위치(carry를 올려주고 1이 된 위치의 인덱스)
     if(p == 0){ //0번 인덱스 위치에서 캐리가 발생했다면 자리수를 늘려야 한다.
-        string value = 
+        string temp = "1";
+        s = temp.append(s);
+        return;
     }
-    char c = s[p-1];
-    switch (c)
-    {
+    switch (s[p-1]){     //carry를 넘겨 받은 위치의 값을 파악한다. -> carry를 넘겨받은 위치의 값을 바꿔준다. 
     case '1':
-        /* code */
+        s[p-1] = '2';
         break;
-    
-    default:
+    case '2':
+        s[p-1] = '4';
+        break;
+    case '4':   //이 경우 carry를 넘겨받았는데 또 carry가 발생하게 되는 경우가 된다. 
+        s[p-1] = '1';
+        proc_carry(s, p-1); //recurcive call
         break;
     }
 }
@@ -25,18 +28,26 @@ string solution(int n) {
     //그 앞의 숫자는 어떻게 정해나가야 하지? cnt변수같은걸 둬서 n값에 이를 때까지 answer string을 바꿔주는 식으로 해야하나
     //1의 자리에서는 숫자 하나를, 10의 자리부분에서는 1의 자리 숫자 3개를, 100의 자리 값은 그 자리값 하나에서 9개의 수를 포함 -> 이걸 이용할 수 있을까?
     //10진법은 0 ~ 9를 넘으면 carry가 발생하고 다시 1의 자리는 0부터 시작하듯이 1, 2, 4 이후에 1의 자리는 1로 돌아오고 carry를 처리하는 방식으로? 
+    //어떤 2진수적인 숫자방식으로 풀 수도 있을까?
 
-    string base[] = {"0", "1", "2", "4"};  //0번 인덱스 값은 쓰지 않음
-    string prev = base[3];  //prev값은 "4"의 값을 가지고 있음
-    string now;
+    string num = "1";
+    if(n == 1) return num;
     int i;
-    for(i = 4; i <= n; i++){    //n까지 계산해 나아간다. 
-        if(prev.back() == '4'){ //이전 수의 맨 뒤의 수가 4였다면 이번 수를 구할 때는 캐리를 올려야 한다.
-            prev.back() == '1';
-
+    char m; //modifying, 변경할 값
+    for(i = 2; i <= n; i++){    //n까지 계산해 나아간다. 
+        switch (num.back()){
+        case '1':
+            m = '2';
+            break;
+        case '2':
+            m = '4';
+            break;
+        case '4':   //이전 수의 맨 뒤의 수가 4였다면 이번 수를 구할 때는 캐리를 올려야 한다.
+            m = '1';
+            proc_carry(num, num.length()-1);
+            break;
         }
+        num.back() = m;
     }
-    string answer = "";
-    
-    return answer;
+    return num;
 }
