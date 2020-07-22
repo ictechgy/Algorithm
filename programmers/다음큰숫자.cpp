ce15e1edc709fc6382f7b7efb81a5a5a65d5bc35
ictechgy <ictechgy@gmail.com>
2020-07-23 01:20:16 +0900

    다음 큰 숫자 문제를 bitset으로 풀어보았다. 다른 풀이가 있을까?

--- programmers/다음큰숫자.cpp
+++ programmers/다음큰숫자.cpp
@@ -7,16 +7,16 @@ using namespace std;
 int solution(int n) {
     int i, j, cnt;
     bitset<32> num(n);
-    i = 31;
-    while(num[i] != 1) i--; //bitset의 오른쪽부터 읽기
-    j = i-1;
-    while(num[j] != 0) j--;
+    i = 0;
+    while(num[i] != 1) i++; //bitset의 오른쪽부터 읽기, bit값 중 1이 최초로 나타나는 위치 찾기  -> 맨 오른쪽이 0번인덱스이고 왼쪽으로 갈 수록 인덱스 값 증가
+    j = i+1;
+    while(num[j] != 0) j++; //i이전 비트 중 0이 최초로 나타나는 위치 찾기
     //맨 오른쪽부터 읽어서 1로 구성된 비트그룹의 위치를 특정해낸다. 
     //해당 비트그룹 중 맨 왼쪽 값만 하나 왼쪽으로 옮겨주고 나머지 1값들은 맨 오른쪽으로 붙이면 그게 다음값이 된다.
     num.set(j, true);
-    cnt = i - j - 1;    //1의 그룹 개수 중 맨 오른쪽으로 몰 1의 개수만을 구하기 위해 1을 더 빼줌
-    for(j = j+1; j < 32; j++) num[j] = 0;   //오른쪽 나머지 비트를 모두 0으로 만든다.
-    for(i = 31; cnt > 0; i--, cnt--) num[i] = 1;    //오른쪽 비트에서 cnt개수만큼 1로 만들기
+    cnt = j - i - 1;    //1의 그룹 개수 중 맨 오른쪽으로 몰 1의 개수만을 구하기 위해 1을 더 빼줌
+    for(j = j-1; j >= 0; j--) num[j] = 0;   //오른쪽 나머지 비트를 모두 0으로 만든다.
+    for(i = 0; cnt > 0; i++, cnt--) num[i] = 1;    //오른쪽 비트에서 cnt개수만큼 1로 만들기
     
     int answer = (int)num.to_ulong();
     
--- programmers/탑.cpp
+++ programmers/탑.cpp
@@ -8,7 +8,7 @@ vector<int> solution(vector<int> heights) {
     //가장 단순한 방법
     int i, j;
     for(i = heights.size()-1; i > 0; i--){  //0번인덱스 안테나는 어차피 0
-        for(int j = i-1; j >= 0; j--){
+        for(j = i-1; j >= 0; j--){
             if(heights[j] > heights[i]) {
                 answer[i] = j + 1;  //~번째이므로 +1
                 break;  //stack 쓰는 경우 여기서 pop시키면 될 듯 
@@ -19,3 +19,5 @@ vector<int> solution(vector<int> heights) {
     return answer;
 }
 //뭐랄까 더 단순하고 좋은 방법 없을까? 위의 방식은 최악의 경우 O(n^2)정도까지 복잡도가 증가할 것 같다. 
+//안테나 n개가 들어왔을 때 맨 뒤의 n번째 안테나에 대하여 더 큰 안테나를 앞에서 찾아야하는데 최악의 경우 첫번째까지 다 봐야 하므로 n-1번 비교 
+//물론 아무리 최악이어도 비교횟수가 n-1, n-2...2, 1로 줄어들 것이고 총 n-1개에 대하여 수행이 되므로 n(n-1)/2 -> O(n^2)
