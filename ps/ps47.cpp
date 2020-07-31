135cd2e314443ed1a6c51fecc722c6081e9429c6
ictechgy <ictechgy@gmail.com>
2020-08-01 00:49:23 +0900

    전역 풀이 추가

--- ps/ps47.cpp
+++ ps/ps47.cpp
@@ -47,3 +47,35 @@ bool checkPeak(vector<vector<int> > &map, int i, int j, vector<vector<int> > &ch
 }
 
 //
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int a[52][52];
+int dx[4] = {-1, 0, 1, 0}; 
+int dy[4] = {0, 1, 0, -1};
+int main(){
+    int n, i, j, k, cnt = 0, flag;
+    scanf("%d", &n);
+    //vector<vector<int> > map(n+2, vector<int>(n+2, 0));
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            flag = 0;
+            for(k = 0; k < 4; k++){
+                if(a[i][j] <= a[i+dx[k]][j+dy[k]]){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0) cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+
+    return 0;
+}
