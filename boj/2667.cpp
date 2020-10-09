91ef16084f401948bf512471cb180c15f905e7ed
ictechgy <ictechgy@gmail.com>
2020-10-09 19:10:14 +0900

    -백준 2667번 단지번호붙이기-
    DFS나 BFS로 풀 수 있는 문제이다. 나는 DFS로 풀었다.
    입력이 다른 문제들과는 조금 다르다. 숫자가 공백을 사이에 두고 들어오는 것이 아니라 한 줄의 숫자들은 연속되어 붙어서 들어온다.
    이에 대해 나는 버퍼와 아스키코드를 이용한 처리를 해주었다.
    
    버퍼의 사이즈가 25인 경우 문제 채점시 '틀렸습니다'가 나온다. 이 부분은 조금 이상하다. 물론 문자열 입력 시 '\n'을 고려하여
    버퍼의 사이즈는 26이상으로 잡는게 좋다. 하지만 인덱스를 통해 개별 값에 접근하기 때문에 맨 뒤에 개행문자가 있든 없든 상관이 없지 않은가?
    (아니면 Map의 크기를 25*25로 잡아서 그런건가? 하지만 그것도 별 문제가 되지는 않을텐데...)
    
    다른 사람들의 풀이를 봤는데 다른사람들은..
    입력값 자체를 문자열(char 또는 string)2차원 배열로 받아서 이를 이용-탐색하기도 하였고 방문 체크 배열을 쓰기도 하였다.
    전체 단지수에 있어서는 나처럼 cnt 변수를 쓰지 않고 바로 vector.size()로 출력해주었다.
    
    가장 신기했던 것은 값을 입력 받을 때 정수로 입력받는데 "%1d"로 입력받은 것..(scanf()사용) '이렇게 서식제어문자 내에 숫자를 이용해서
    원하는 방식으로 입력받는 것도 가능하구나'를 알 수 있었다.

---
+++ boj/2667.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Map[25][25], n, cnt = 0, area;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+vector<int> district;
+
+void DFS(int x, int y){
+    for(int i = 0; i < 4; i++){
+        int xx = x + dx[i];
+        int yy = y + dy[i];
+        if(xx >= 0 && xx < n && y >= 0 && yy < n && Map[xx][yy] == 1){
+            area++;
+            Map[xx][yy] = 0;
+            DFS(xx, yy);
+        }
+    }
+}
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char buffer[25];
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> buffer;
+        for(int j = 0; j < n; j++){
+            Map[i][j] = buffer[j] - 48;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(Map[i][j] == 1){
+                cnt++;
+                area = 1;
+                Map[i][j] = 0;
+                DFS(i, j);
+                district.push_back(area);
+            }
+        }
+    }
+    cout << cnt << "\n";
+    sort(district.begin(), district.end());
+    for(int i = 0; i < district.size(); i++){
+        cout << district[i] << "\n";
+    }
+
+    return 0;
+}
