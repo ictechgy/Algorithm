5be8bbd4bc252fe0b0c8f272eae3591c9257d259
ictechgy <ictechgy@gmail.com>
2020-10-24 22:17:08 +0900

    설마설마 했는데 자릿수가 아닌 앞자리 0같은 것들도 있는 것처럼 처리하는게 맞다고 함..

--- boj/9019.cpp
+++ boj/9019.cpp
@@ -87,3 +87,67 @@ int main(void){
 
     return 0;
 }
+
+/*
+혹시나 해서인데 계속 7%대에서 틀리는 이유가.. 
+A가 1, B가 10일 때
+결과 값으로는 DDSDSD가 나오는데
+A값을 0001로 준거여서.. 정답이 L 하나만 나와야 한다는건가..?
+0001 -> 0010 ..?
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int T, A, B;
+
+string multiply(string s){
+    int n = (stoi(s)*2)%10000;
+    s = to_string(n);
+    int _size = s.size();
+    for(int i = 1; i <= 4-_size; i++) s.insert(0, "0"); //앞자리에 빈 공간만큼 0을 채워줌
+    return s;
+}
+
+string subtract(string s){
+    int n = stoi(s)-1;
+    if(n < 0) n = 9999;
+    s = to_string(n);
+    int _size = s.size();
+    for(int i = 1; i <= 4-_size; i++) s.insert(0, "0");
+    return s;
+}
+
+string left(string s){
+    string s2 = s.substr(1);
+    s2.append(to_string(s[0]));
+    return s2;
+}
+
+string right(string s){
+    char back = s.back();
+    s.pop_back();
+    s = back + s;
+    return s;
+}
+
+struct Register{
+    string num;
+    string command;
+    Register(string n, string s){
+        num = n;
+        command = s;
+    }
+};
+
+//작성하다보니 든 생각인데 이렇게 string으로 자릿수와 상관없는 0도 살려서 처리하는게 맞는걸까? 
+//예제입력에서는 A와 B 다 0001처럼 주어지지는 않는거같은데
+//너무 모르겠어서 설마설마 하고 질문글 쪽을 봤는데.. 0을 살려서 보는게 맞는 듯 하다.
+//123을 L하면 231이 아니라 1230이라고 한다... 
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    return 0;
+}
