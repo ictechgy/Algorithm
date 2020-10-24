f751e723d682e87b69e4c340107b579590d4704a
ictechgy <ictechgy@gmail.com>
2020-10-24 17:29:30 +0900

    레퍼런스를 쓰면 런타임오류, 안쓰면 틀렸다고 나오는데 둘 다 이유를 모르겠다.. 반례가 있으려나?

--- boj/9019.cpp
+++ boj/9019.cpp
@@ -37,7 +37,7 @@ int right(int n){
 struct Register{
     int num;
     string command;
-    Register(int n, const string &s){
+    Register(int n, /*const string &s*/ string s){
         num = n;
         command = s;
     }
@@ -46,34 +46,42 @@ struct Register{
 int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    bool ck[10000];     //큐에 이미 들어갔던 숫자는 다시 넣지 않기 위한 체크배열
     
     cin >> T;
     for(int i = 1; i <= T; i++){
         cin >> A >> B;
-        queue<Register> q;
+        memset(ck, 0, sizeof(ck));
+
+        queue<Register> q;      //Queue는 별도로 clear함수가 없어서 외부에서 선언해서 재사용할 수가 없구나..
         q.push(Register(A, ""));
+        ck[A] = true;
+
         while(!q.empty()){
-            Register &node = q.front();
+            //Register &node = q.front();
+            Register node = q.front();
             q.pop();
             int n = node.num;
-            string &s = node.command;
+            //string &s = node.command;
+            string s = node.command;
 
             int after;
             after = multiply(n);
             if(after == B) {cout << s + "D" << "\n"; break;}
-            if(after != A) q.push(Register(after, s + "D"));
+            if(!ck[after]) {q.push(Register(after, s + "D")); ck[after] = true;}
 
             after = subtract(n);
             if(after == B) {cout << s + "S" << "\n"; break;}
-            if(after != A) q.push(Register(after, s + "S"));
+            if(!ck[after]) {q.push(Register(after, s + "S")); ck[after] = true;}
 
             after = left(n);
             if(after == B) {cout << s + "L" << "\n"; break;}
-            if(after != A) q.push(Register(after, s + "L"));
+            if(!ck[after]) {q.push(Register(after, s + "L")); ck[after] = true;}
 
             after = right(n);
             if(after == B) {cout << s + "R" << "\n"; break;}
-            if(after != A) q.push(Register(after, s + "R"));
+            if(!ck[after]) {q.push(Register(after, s + "R")); ck[after] = true;}
         }
     }
 
