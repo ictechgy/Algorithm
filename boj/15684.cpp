201632af039a6ebff8ee907b2c62a5697b258bdc
ictechgy <ictechgy@gmail.com>
2020-11-03 22:20:21 +0900

    왜 계속 시간초과가 날까

--- boj/15684.cpp
+++ boj/15684.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int N, M, H;
 int ladder[31][11];
 int MIN = 2147000000;
+bool isConnected[11];   //특정 열의 사다리가 다른 사다리랑 연결되어있는지 체크 -> 이걸로 가지치기가 가능할까? 
+//아무것도 연결 안되어있는 사다리 열은 그대로 지나치는게 전체적으로 더 빠른 속도를 낼까?
+//이전에 git commit message에서 100 0 100이 오래걸린다 했는데 이는 배열 인덱스 값을 넘어선 값이라 결과 값 도출이 제대로 안된거같고.
+//M이 0일 때 바로 0을 출력하는 구문을 없애고 10 0 30으로 N과 H의 최대치 해봤는데 시간이 오래걸린다. 가지치기가 필요한 듯
+
+//isConnected에 cnt를 이용한 가지치기를 추가했는데도 시간초과가 뜬다 ㅠㅠㅠㅠㅠ 
 
 //사다리의 왼쪽에서 오른쪽으로 훑어가면서 연결가능한 부위가 있다면 연결도 해보고 연결안함도 해보면서 다음 DFS호출
 void DFS(int y, int x, int cnt){ 
@@ -19,11 +25,17 @@ void DFS(int y, int x, int cnt){
         if(MIN > cnt) MIN = cnt;
     }else if(x >= N){    //특정 가로줄의 맨 끝에 도달 시
         DFS(y+1, 1, cnt);    //그 다음줄 맨 왼쪽으로 조정
+    }else if(isConnected[x] == false || isConnected[x+1] == false){
+        DFS(y, x+1, cnt);
     }else{
         if(ladder[y][x] == x && ladder[y][x+1] == x+1){  //주변 사다리와 연결 안된상태 && 오른쪽 사다리도 다른 사다리와 연결 안된 상태
-            ladder[y][x] = x + 1;
-            ladder[y][x+1] = x; //쌍방연결
-            DFS(y, x+2, cnt+1); //x+1은 볼 필요 없음
+            
+            if(cnt < MIN){
+                ladder[y][x] = x + 1;
+                ladder[y][x+1] = x; //쌍방연결
+                DFS(y, x+2, cnt+1); //x+1은 볼 필요 없음
+            }   
+            //가지치기. cnt가 MIN보다 작은경우에만 연결해서 DFS깊이를 더 들어갈 필요가 있음. cnt가 이미 MIN과 같거나 큰 경우 더 연결해서 들어가볼 필요 x
 
             ladder[y][x] = x;
             ladder[y][x+1] = x+1;   //백트래킹
@@ -40,6 +52,10 @@ int main(void){
     int a, b;
     cin >> N >> M >> H;
     //N은 세로선의 수, M은 현재 연결되어있는 가로선 수, H는 가로선을 둘 수 있는 라인 수
+    if(M == 0){
+        cout << 0;
+        return 0;
+    }
 
     //사다리 구조를 int형 2차원 배열로 만들자.
     //1번에서 2번으로 가는 길이 1번가로줄에 있다고 가정한다면 이는
@@ -57,6 +73,9 @@ int main(void){
         cin >> a >> b;  
         ladder[a][b] = b+1;
         ladder[a][b+1] = b;
+
+        isConnected[b] = true;
+        isConnected[b+1] = true;
     }
     
     //모든 경우를 다 탐색한 이후에 가장 적은 값이 3을 초과하는지 아닌지에 따라서 값 출력
